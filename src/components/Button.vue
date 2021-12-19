e7dc29a03b5a255e8b372c647ebb6366f7c384d6
syp1996 <304899670@qq.com>
2021-12-19 21:31:47 +0800

    button添加

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,29 +1,40 @@
 <template>
-  <div class="hello">
-    <my-Button>默认按钮</my-Button>
-    <my-Button type="primary">主要按钮</my-Button></my-Button>
-    <my-Button type="success">成功按钮</my-Button>
-    <my-Button type="info">信息按钮</my-Button>
-    <my-Button type="warning">警告按钮</my-Button>
-    <my-Button type="danger">危险按钮</my-Button>
-  </div>
+<div class="container">
+    <div>
+        <my-Button>默认按钮</my-Button>
+        <my-Button type="primary">主要按钮</my-Button>
+        <my-Button type="success">成功按钮</my-Button>
+        <my-Button type="info">信息按钮</my-Button>
+        <my-Button type="warning">警告按钮</my-Button>
+        <my-Button type="danger">危险按钮</my-Button>
+    </div>
+    <div style="margin-top:20px">
+        <my-Button>默认按钮</my-Button>
+        <my-Button type="primary" plain>主要按钮</my-Button>
+        <my-Button type="success" plain>成功按钮</my-Button>
+        <my-Button type="info" plain>信息按钮</my-Button>
+        <my-Button type="warning"plain>警告按钮</my-Button>
+        <my-Button type="danger" plain>危险按钮</my-Button>
+    </div>
+</div>
 </template>
 
-<script  >
+<script>
 export default {
-  name: 'button',
-  created(){
+    name: 'button',
+    created() {
 
-  },
-  data () {
-    return {
+    },
+    data() {
+        return {
 
+        }
     }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style lang="scss"  scoped>
 
 </style>
